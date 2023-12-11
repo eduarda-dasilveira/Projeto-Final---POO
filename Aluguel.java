9c220cbfd57a60db7536c69480f616621d70b80a
Eduarda Menezes da Silveira <122768211+eduarda-dasilveira@users.noreply.github.com>
2023-12-11 02:00:12 -0300

    Update Aluguel.java

--- Aluguel.java
+++ Aluguel.java
@@ -1,25 +1,25 @@
 import java.util.Date;
 
 public class Aluguel {
-    int codigoLivro;
-    Date dataAluguel;
-    Date dataDevolucao;
-    String nomeLivro;
-    int matriculaCliente;
+	int codigoLivro;
+	Date dataAluguel;
+	Date dataDevolucao;
+	String nomeLivro;
+	int matriculaCliente;
 
-    public Aluguel(int codigoLivro, Date dataAluguel, Date dataDevolucao, String nomeLivro, int matriculaCliente) {
-        this.codigoLivro = codigoLivro;
-        this.dataAluguel = dataAluguel;
-        this.dataDevolucao = dataDevolucao;
-        this.nomeLivro = nomeLivro;
-        this.matriculaCliente = matriculaCliente;
-    }
+	public Aluguel(int codigoLivro, Date dataAluguel, Date dataDevolucao, String nomeLivro, int matriculaCliente) {
+		this.codigoLivro = codigoLivro;
+		this.dataAluguel = dataAluguel;
+		this.dataDevolucao = dataDevolucao;
+		this.nomeLivro = nomeLivro;
+		this.matriculaCliente = matriculaCliente;
+	}
 
-    public Date getDataAluguel() {
-        return this.dataAluguel;
-    }
+	public Date getDataAluguel() {
+		return this.dataAluguel;
+	}
 
-    public Date getDataDevolucao() {
-        return this.dataDevolucao;
-    }
+	public Date getDataDevolucao() {
+		return this.dataDevolucao;
+	}
 }
